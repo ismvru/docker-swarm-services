cce2516fa51732a80d116319e41bf73bdee99339
Mikhail Isaev <isaevmv@it.mos.ru>
2022-05-23 14:09:36 +0300

    Add bootstrap, buttons...

--- static/table.js
+++ static/table.js
@@ -2,6 +2,21 @@ $(document).ready(
     function () {
         $('#table_id').DataTable(
             {
+                dom: 'Bfrtip',
+                buttons: [
+                    'copyHtml5', 'csvHtml5', 'excelHtml5', 'pdfHtml5',
+                    {
+                        extend: 'print',
+                        exportOptions: {
+                            columns: ':visible'
+                        }
+                    },
+                    {
+                        extend: 'colvis',
+                        collectionLayout: 'fixed columns',
+                        collectionTitle: 'Column visibility control'
+                    }
+                ],
                 ajax: '/ajax',
                 columns: [
                     { data: 'short_id' },
